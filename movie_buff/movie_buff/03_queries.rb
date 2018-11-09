1351a1566f12aaec292c665011224f0b7f321297
rikeychen <steveninouye@msn.com>
2018-11-08 17:30:06 -0800

    Update 03_queries.rb

--- movie_buff/movie_buff/03_queries.rb
+++ movie_buff/movie_buff/03_queries.rb
@@ -2,7 +2,7 @@ def what_was_that_one_with(those_actors)
   # Find the movies starring all `those_actors` (an array of actor names).
   # Show each movie's title and id.
 
-  Movie.select(:title, :id).joins(:actors).where(castings: {ord: 1}, name: :those_actors)
+  Movie.select(:title, :id).joins(:actors).where(castings: {ord: 1}, actors: {name: :those_actors})
 end
 
 def golden_age
